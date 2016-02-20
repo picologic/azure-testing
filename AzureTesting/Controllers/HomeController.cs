c24e29f0867dda5f2004868a148928c984ae4264
Glen Cooper <glenc@pico-logic.com>
2016-02-20 12:03:18 -0800

    Changed to different .net version

--- AzureTesting/Controllers/HomeController.cs
+++ AzureTesting/Controllers/HomeController.cs
@@ -1,34 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNet.Mvc;
+using System.Web;
+using System.Web.Mvc;
 
 namespace AzureTesting.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
-        {
+        public ActionResult Index() {
             return View();
         }
 
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
+        public ActionResult About() {
+            ViewBag.Message = "Your application description page.";
 
             return View();
         }
 
-        public IActionResult Error()
-        {
+        public ActionResult Contact() {
+            ViewBag.Message = "Your contact page.";
+
             return View();
         }
     }
---
+++ AzureTesting/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AzureTesting.MvcApplication" Language="C#" %>
